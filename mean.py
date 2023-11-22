f3351f5cebe5c2c382862be44899f7bdba5e1c17
bhargav0425 <patelbhargav2510@gmail.com>
2023-11-22 16:57:25 -0500

    Final Implematation - Removed Unused Files

--- mean.py
+++
@@ -1,9 +0,0 @@
-class Mean:
-    @staticmethod
-    def calculate(data):
-        if not data:
-            raise ValueError("Data is empty")
-        total = 0
-        for x in data:
-            total += x
-        return total / len(data)
